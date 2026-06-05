1f74d5370664db428f1bc506b2c1df2aa2723b00
Xander <xanderbholden@gmail.com>
2026-06-05 16:42:25 -0600

    Added root API route

--- routes/index.js
+++ routes/index.js
@@ -4,3 +4,10 @@ router.use('/cars', require('./cars'));
 router.use('/events', require('./events'));
 
 module.exports = router;
+
+router.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Car Meet API',
+    docs: '/api-docs'
+  });
+});
